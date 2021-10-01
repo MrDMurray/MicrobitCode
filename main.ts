ebd6410c4250791e6ccdfcf3897267231b9e89bf
Daniel Murray <daniel.murray@stjosephsrush.ie>
2021-10-01 16:25:02 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
-basic.showLeds(`
-    . . # . .
-    . # # # .
-    # # # # #
-    . . # . .
-    . . # . .
-    `)
-let angle = 90
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        # # # # #
+        . # # . .
+        . . # . .
+        `)
+    pins.servoWritePin(AnalogPin.P13, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    pins.servoWritePin(AnalogPin.P13, 90)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # # .
+        # # # # #
+        . . # # .
+        . . # . .
+        `)
+    pins.servoWritePin(AnalogPin.P13, 180)
+})
